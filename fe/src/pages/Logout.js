35db797279cd04327e8f1395736887bacec35e9a
[Github <[Github>
2022-10-26 18:14:28 +0900

    style: 구조 생성

--- fe/src/pages/Logout.js
+++ fe/src/pages/Logout.js
@@ -1,7 +1,62 @@
 import React from "react";
+import styled from "styled-components";
 
 function Logout() {
-  return <div>Logout</div>;
+  return (
+    <Display>
+      <Wraper>
+        <div>
+          Clicking “Log out” will log you 
+          out of the following domains on this device:
+        </div>
+        <div>
+          <div>
+            <div>아이콘</div>
+            <div>askubuntu.com</div>
+          </div>
+          <div>
+            <div>아이콘</div>
+            <div>askubuntu.com</div>
+          </div>
+          <div>
+            <div>아이콘</div>
+            <div>askubuntu.com</div>
+          </div>
+          <div>
+            <div>아이콘</div>
+            <div>askubuntu.com</div>
+          </div>
+          <div>
+            <div>아이콘</div>
+            <div>askubuntu.com</div>
+          </div>
+          <div>
+            <div>아이콘</div>
+            <div>askubuntu.com</div>
+          </div>
+          <div>
+            <div>아이콘</div>
+            <div>askubuntu.com</div>
+          </div>
+          <div> ---------------- </div>
+        </div>
+      </Wraper>
+    </Display>  
+  )
 }
+
+
+const Display = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 2px solid red;
+`
+
+const Wraper = styled.div`
+  width: 65vw;
+  height: 100vh;
+  border: 2px solid blue;
+`
+
 
 export default Logout;
